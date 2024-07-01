79e58435c60db81f32cf079a2edbc21cf42d1048
vijayammanagi1234 <48375309+vijayammanagi1234@users.noreply.github.com>
2024-07-01 13:52:45 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, Baby how are you....");
-    System.out.println("Hello, Baby how are you....");
-    System.out.println("Hello, Baby how are you....");
-    System.out.println("Hello, Baby how are sdshdghj.");
-
+    System.out.println("Hello, Today is the Monday.");
   }
 }
 
