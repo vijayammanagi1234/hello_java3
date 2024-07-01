842e83f213776441b57d458f547a9c4ecb9ac6e0
Ubuntu <ubuntu@ip-172-31-47-188.ap-south-1.compute.internal>
2024-07-01 08:53:43 +0000

    we have update the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, Today is the Monday.");
     System.out.println("Hello, tomorrow is the Tuesday.");
-    System.out.println("Hello, tomorrow is the wednesday.");  }
+    System.out.println("Hello, tomorrow is the wednesday."); 
+     System.out.println("Hello, tomorrow is the thurday.");
+  }
 }
 
