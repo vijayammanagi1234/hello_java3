18f89d8bbadadc4afeddc4f30ed39eee8f9bb39c
Ubuntu <ubuntu@ip-172-31-47-188.ap-south-1.compute.internal>
2024-07-01 08:50:07 +0000

    we have update next after day

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, Today is the Monday.");
-    System.out.println("Hello, tomorrow is the Monday.");
-  }
+    System.out.println("Hello, tomorrow is the Tuesday.");
+    System.out.println("Hello, tomorrow is the wednesday.");  }
 }
 
